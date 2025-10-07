4ad9cfbf60fbc4872c4b4f406a21ae5bbaf96e2d
alexandersaadaoui <105090016+alexandersaadaoui@users.noreply.github.com>
2025-10-07 05:01:28 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+function showAlert() {
+    alert('Hello! Thank you for playing my short, text-based game, True or False! This is based on the reality TV show "Moment of Truth". Images are from shutterstock and audio is from pixabay. Hint: 211112');
+}
+
+function start() {
+    var game = 'question1.html';
+    window.location.href = game;
+}
+
+function start2() {
+    var game = 'question2.html';
+    window.location.href = game;
+}
+
+function start3() {
+    var game = 'question3.html';
+    window.location.href = game;
+}
+
+function start4() {
+    var game = 'question4.html';
+    window.location.href = game;
+}
+
+function start5() {
+    var game = 'question5.html';
+    window.location.href = game;
+}
+
+function start6() {
+    var game = 'question6.html';
+    window.location.href = game;
+}
+
+
+function end() {
+    var game = 'theend.html';
+    window.location.href = game;
+}
+
+function lose() {
+    var game = 'incorrect.html';
+    window.location.href = game;
+}
+
+function restart() {
+    var game = 'index.html';
+    window.location.href = game;
+}
+
+function playAudio(audioSrc) {
+    var audioPlayer = document.getElementById('audio-player');
+    audioPlayer.src = audioSrc;
+    audioPlayer.play();
+}
